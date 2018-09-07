fec8c5962883391ebc1f2d0beb9d04aa3b6bf901
Junior Aguilar <cafu.aguilar@tecsup.edu.pe>
2018-09-06 23:14:09 -0500

    Modificacion

--- src/main/java/com/tecsup/lab03/lab03_4C15_2018_1_JAB/StudentCotnroller.java
+++ src/main/java/com/tecsup/lab03/lab03_4C15_2018_1_JAB/StudentCotnroller.java
@@ -1,5 +1,8 @@
 package com.tecsup.lab03.lab03_4C15_2018_1_JAB;
 
 public class StudentCotnroller {
-
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
